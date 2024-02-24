d9afe05901e3347fb17332de02f3e1d36ef8b81f
Christy-Lin <cl5501@nyu.edu>
2024-02-24 12:37:36 -0500

    Moved backend url into env var

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,3 @@
-export const BACKEND_URL = 'http://127.0.0.1:8000/';
+export const BACKEND_URL = process.env.REACT_APP_URL_PRE;
+// export const BACKEND_URL = 'http://127.0.0.1:8000/';
 // export const BACKEND_URL = 'http://localhost:3000';
