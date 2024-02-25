017730bf60b9ca597f3ff3b446ae337ce0622e52
Nishma <ns4606@nyu.edu>
2024-02-25 15:01:42 -0500

    adding fallback in constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env.REACT_APP_URL_PRE;
+export const BACKEND_URL = process.env.REACT_APP_URL_PRE || "http://127.0.0.1:8000/";
 // export const BACKEND_URL = 'http://127.0.0.1:8000/';
 // export const BACKEND_URL = 'http://localhost:3000';
