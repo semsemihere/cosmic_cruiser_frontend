c7307c9caddf7717857390fd7747b7023738739f
Nishma <ns4606@nyu.edu>
2024-02-29 21:15:42 -0500

    updated Users.jsx to get post endpoint working

--- frontend/src/Components/Users/Users.jsx
+++ frontend/src/Components/Users/Users.jsx
@@ -6,17 +6,34 @@ import { BACKEND_URL } from '../../constants';
 const USERS_ENDPOINT = `${BACKEND_URL}/users`;
 
 function AddUserForm({setError, fetchUsers}) {
-  const [name, setName] = useState('')
   const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [firstname, setFirstName] = useState('')
+  const [lastname, setLastName] = useState('')
+  const [phonenumber, setPhone] = useState(0);
+  const [role, setRole] = useState('');
 
-  const changeName = (event) => { setName(event.target.value); };
   const changeEmail = (event) => { setEmail(event.target.value); };
+  const changeUsername = (event) => { setUsername(event.target.value); };
   const changePassword = (event) => { setPassword(event.target.value); };
+  const changeFirstName = (event) => { setFirstName(event.target.value); };
+  const changeLastName = (event) => { setLastName(event.target.value); };
+  const changePhone = (event) => { setPhone(event.target.value); };
+  const changeRole = (event) => { setRole(event.target.value); };
+
+  // FROM BACKEND:
+  // new_user = users.create_user(
+  //   email, username,
+  //   password, firstname,
+  //   lastname, phonenumber
+  // )
+  // INFO TO CREATE USER: email, username, password, firstname, lastname, phonenumber
 
   const addUser = (event) => {
     event.preventDefault();
-    axios.post(USERS_ENDPOINT, { name: name, email: email, password: password })
+    axios.post(USERS_ENDPOINT, { email: email, username: username, password: password, 
+      firstname: firstname, lastname: lastname, phonenumber: phonenumber, role: role })
     .then(() => {  // if successful
       setError('');
       fetchUsers();
@@ -25,14 +42,9 @@ function AddUserForm({setError, fetchUsers}) {
   };
 
 
+  // INFO TO CREATE USER: email, username, password, firstname, lastname, phonenumber
   return (
     <form>
-
-      <label htmlFor='name'>
-        Name
-      </label>
-      <input type="text" id="name" value={name} onChange={changeName}>
-      </input>
 
       <label htmlFor='email'>
         Email
@@ -40,11 +52,42 @@ function AddUserForm({setError, fetchUsers}) {
       <input type="text" id="name" value={email} onChange={changeEmail}>
       </input>
 
-      <label htmlFor='email'>
+      <label htmlFor='username'>
+        Username
+      </label>
+      <input type="text" id="username" value={username} onChange={changeUsername}>
+      </input>
+
+      <label htmlFor='password'>
         Password
       </label>
-      <input type="text" id="name" value={password} onChange={changePassword}>
+      <input type="text" id="password" value={password} onChange={changePassword}>
       </input>
+
+      <label htmlFor='firstname'>
+        First Name
+      </label>
+      <input type="text" id="firstname" value={firstname} onChange={changeFirstName}>
+      </input>
+
+      <label htmlFor='lastname'>
+        Last Name
+      </label>
+      <input type="text" id="lastname" value={lastname} onChange={changeLastName}>
+      </input>
+
+      <label htmlFor='phonenumber'>
+        Phone Number
+      </label>
+      <input type="number" id="phonenumber" value={phonenumber} onChange={changePhone}>
+      </input>
+
+      <label htmlFor='role'>
+        Role
+      </label>
+      <input type="text" id="role" value={role} onChange={changeRole}>
+      </input>
+
       <button type="submit" onClick={addUser}>Submit</button>
     </form>
   );
